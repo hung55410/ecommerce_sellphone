459f441734fc9801354d6f2854c34230d14f4660
nguyenhung <hung55410@gmail.com>
2023-09-13 01:10:44 +0700

    Update cart activity UI

---
+++ app/src/main/java/com/example/ecommerce_sellphone/Activity/CartActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ecommerce_sellphone.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ecommerce_sellphone.R
+
+class CartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart)
+    }
+}
